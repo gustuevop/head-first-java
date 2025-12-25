87a745f4dc7f07a98cec45082bd1950e5874934e
Gustavo Prado <gustavo.spro@outlook.com>
2025-12-25 09:24:19 -0300

    Party.java

---
+++ Party.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.awt.event.*;
+
+
+class Party {
+
+  public static void main(String[] args) {
+    buildInvite();
+  }
+
+  public void buildInvite() {
+    Frame f = new Frame();
+    Label l = new Label("Party at Tim's");
+    Button b = new Button("You bet");
+    Button c = new Button("Shoot me");
+    Panel p = new Panel();
+    p.add(l);
+    p.add(b);
+    p.add(c);
+    f.add(p);
+  }
+
+}
